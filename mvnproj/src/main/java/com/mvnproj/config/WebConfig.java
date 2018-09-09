813c4055c2e248a7e81f5f9c1da1e6a2fd856926
SStefanovskyi <sstefanovskiy@gmail.com>
2018-09-09 10:50:14 +0300

    Created User class and some beans

--- mvnproj/src/main/java/com/mvnproj/config/WebConfig.java
+++ mvnproj/src/main/java/com/mvnproj/config/WebConfig.java
@@ -21,7 +21,6 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         freeMarkerViewResolver.setSuffix(".ftl");
         freeMarkerViewResolver.setPrefix("");
         return freeMarkerViewResolver;
-
     }
 
     @Bean
